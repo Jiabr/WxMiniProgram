3dc8cf1089131cdf1cabc861e5bc49b552f22587
Jiabr <15720610980@163.com>
2022-10-12 22:58:05 +0800

    20221012

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,6 +4,11 @@ const app = getApp()
 
 Page({
   data: {
-    
+    items: [
+      {value: 'ATunnelStimulate', name : 'A通道刺激'},
+      {value: 'ABTunnelStimulate', name : 'AB通道刺激'},
+      {value: 'ABTunnelPreStimulate', name : 'AB通道预刺激'},
+      {value: 'SignalCollection', name : '采集信号'},
+    ]
   }
 })

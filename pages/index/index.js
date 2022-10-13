ad9841975d0c3843f023647ddd9536e97542a32c
Jiabr <15720610980@163.com>
2022-10-13 21:12:03 +0800

    20221013

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,10 +5,24 @@ const app = getApp()
 Page({
   data: {
     items: [
-      {value: 'ATunnelStimulate', name : 'A通道刺激'},
-      {value: 'ABTunnelStimulate', name : 'AB通道刺激'},
-      {value: 'ABTunnelPreStimulate', name : 'AB通道预刺激'},
-      {value: 'SignalCollection', name : '采集信号'},
+      {checked : true, name : "A通道刺激"},
+      {checked : false, name : "AB通道刺激"},
+      {checked : false, name : "AB通道预刺激"},
+      {checked : false, name : "采集信号"}
     ]
+  },
+  selectHandler(e){
+    this.setData({
+      'items[0].checked' : false, 
+      'items[1].checked' : false, 
+      'items[2].checked' : false, 
+      'items[3].checked' : false
+    });
+    switch(e.target.dataset.index){
+      case "0": this.setData({'items[0].checked' : true}); break;
+      case "1": this.setData({'items[1].checked' : true}); break;
+      case "2": this.setData({'items[2].checked' : true}); break;
+      case "3": this.setData({'items[3].checked' : true});
+    }
   }
 })
